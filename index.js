080f4d448dc54192aae8bbf02ccba9651df38568
Andrey Lavrenuk <andrey.l@appstockus.com>
2021-06-05 16:23:42 +0300

    env providing

--- index.js
+++ index.js
@@ -3,20 +3,28 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const http = require('http');
 const fs = require('fs');
-const vgmUrl =
-  "https://www.olx.ua/nedvizhimost/kvartiry-komnaty/arenda-kvartir-komnat/kvartira/donetsk/?search%5Bfilter_float_number_of_rooms%3Afrom%5D=2&search%5Bfilter_float_number_of_rooms%3Ato%5D=2&search%5Bdistrict_id%5D=23";
+require('dotenv').config();
+
+
+const vgmUrl = process.env.OLX_URL;
+/**
+ * Getting array with OLX items to compare with new parsed items
+ */
+const beginningMessage = process.env.BEGINNING_MESSAGE;
 const appartaments = require('./test.json');
 const telegramBot = require('node-telegram-bot-api');
-const botToken = '1709145377:AAEeX_iAGKbyui5_gKDmjDDuak6lr5TpYAw';
+const botToken = process.env.TELEGRAM_TOKEN;
 const bot = new telegramBot(botToken,{polling:true});
+
+
 bot.onText(/бот, ищи хаты/, (msg, match) => {
 
 	const chatId = msg.chat.id
 
-	bot.sendMessage(chatId, 'Заебали вы кожаные мешки нахуй, сами нихуя не можете, мне за вас отдуваться надо блять. Ладно, приступаю к поиску ебать');
+	bot.sendMessage(chatId, beginningMessage);
 /**
  * Yeah, `setInterval`, because I was lazy to wrap this app in docker container and configure cronjobs. 
- * But, dear contributor, if you want - its up  to you!
+ * But, dear contributor, if you want - its up to you!
  */
   setInterval(()=>{
     got(vgmUrl)
@@ -25,19 +33,30 @@ bot.onText(/бот, ищи хаты/, (msg, match) => {
       dom.window.document
         .querySelectorAll(".offer-wrapper")
         .forEach((element) => {
+          /**
+           * Getting olx item
+           */
           let link = element.querySelector("h3").querySelector("a").getAttribute("href");
-          if (!link.includes('promoted') && !appartaments.includes(link)) {
 
+          if (!link.includes('promoted') && !appartaments.includes(link)) {
+            /**
+             * If item not promoted and its new (not includes in appartaments array) - pushing it to appartaments
+             */
             appartaments.push(
               link
             );
             console.log(link)
+
             bot.sendMessage(chatId, link);
+
           }else{
             return;
           }
           
         });
+        /**
+         * Rewriting appartaments to file for next OLX-items comparing
+         */
         fs.writeFileSync ("test.json", JSON.stringify(appartaments), function(err) {
           if (err) throw err;
           console.log('complete');
@@ -46,7 +65,7 @@ bot.onText(/бот, ищи хаты/, (msg, match) => {
     .catch((err) => {
       console.log(err);
     });
-    console.log('minute left')
+    console.log('a minute has passed')
   },60000);
 })
 
